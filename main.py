2c27a98c13b969626ef496085e91e6a00d2d4e5c
lutre69 <maxime.ertul@gmail.com>
2017-05-18 17:45:57 +0200

    Regrouped everything into 2 files

--- main.py
+++ main.py
@@ -2,8 +2,27 @@
 from kivy.app import App
 from kivy.properties import ObjectProperty, ListProperty
 from kivy.storage.jsonstore import JsonStore
-from properties import Student, Skill
-from root import Root
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Skill(object):
+    def __init__(self, skill_id, **kwargs):
+        self.skill_id = skill_id
+        self.set_name = kwargs.get('set_name')
+        self.title = kwargs.get('title')
+        self.summary = kwargs.get('summary')
+
+
+class Student(object):
+    def __init__(self, student_id, **kwargs):
+        self.student_id = student_id
+        self.student_name = kwargs.get('name')
+        self.student_surname = kwargs.get('surname')
+        self.student_class = kwargs.get('class')
+
+
+class Root(BoxLayout):
+    pass
 
 
 class ProjectZApp(App):
--- root.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from kivy.uix.boxlayout import BoxLayout
-
-
-class Root(BoxLayout):
-
-    def __init__(self, **kwargs):
-        super().__init__()
-
