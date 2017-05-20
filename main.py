77371f79705012ed8df55bddad961dcaad56600b
Maxime Ertul <maxime.ertul@gmail.com>
2017-05-20 21:38:50 +0200

    Implemented the skills screen button functions

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from kivy.app import App
 from kivy.properties import ObjectProperty, ListProperty
 from kivy.storage.jsonstore import JsonStore
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import Screen
 
 
 class Skill(object):
@@ -17,10 +20,10 @@ class Skill(object):
 
 class Student(object):
     def __init__(self, student_id, **kwargs):
-        self.student_id = student_id
-        self.student_name = kwargs.get('name')
-        self.student_surname = kwargs.get('surname')
-        self.student_class = kwargs.get('class')
+        self.id_ = student_id
+        self.name = kwargs.get('name')
+        self.surname = kwargs.get('surname')
+        self.class_ = kwargs.get('class')
 
 
 class CustomDropDown(DropDown):
@@ -29,6 +32,46 @@ class CustomDropDown(DropDown):
 
 class StandardButton(Button):
     pass
+
+
+class SkillsScreen(Screen):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class SkillsPopupLayout(GridLayout):
+    def __init__(self, app, skills_list, **kwargs):
+        super().__init__(**kwargs)
+        self.app = app
+        self.skills_list = skills_list
+        self.btn_dict = {}
+        for s in self.skills_list:
+            button = Button(text=s.title)
+            self.add_widget(button)
+            button.bind(on_release=self.select)
+            self.btn_dict[button] = s
+
+    def select(self, obj):
+        self.app.selected_skill = self.btn_dict[obj]
+        self.parent.parent.parent.dismiss()
+
+
+class StudentPopupLayout(GridLayout):
+    def __init__(self, app, student_list, **kwargs):
+        super().__init__(**kwargs)
+        self.app = app
+        self.student_list = student_list
+        self.btn_dict = {}
+        for s in self.student_list:
+            button = Button(text="{} {}".format(s.surname, s.name))
+            self.add_widget(button)
+            button.bind(on_release=self.select)
+            self.btn_dict[button] = s
+
+    def select(self, obj):
+        self.app.selected_student = self.btn_dict[obj]
+        self.parent.parent.parent.dismiss()
 
 
 class MenuButton(Button):
@@ -44,7 +87,8 @@ class MenuButton(Button):
         instance.clear_widgets()
         for x in screen_list:
             if x != value:
-                button = Button(text=x, size_hint_y=None, height="44dp")
+                button = Button(text=x, size_hint_y=None, height="44dp",
+                                background_color=[1, 1, 1, 0.6])
                 button.bind(on_release=lambda btn: instance.select(btn.text))
                 instance.add_widget(button)
 
@@ -67,6 +111,8 @@ class ProjectZApp(App):
     data_output = ObjectProperty(JsonStore('data/outputs.json'))
     student_list = ListProperty()
     skills_list = ListProperty()
+    selected_student = ObjectProperty()
+    selected_skill = ObjectProperty()
 
     def build(self):
         self.student_list = self.get_students()
@@ -88,6 +134,46 @@ class ProjectZApp(App):
             file.write(data)
             file.close()
         return path
+
+    def open_student_popup(self):
+        Popup(content=StudentPopupLayout(self,
+              self.student_list,
+              cols=2),
+              title='Faites votre choix',
+              auto_dismiss=False).open()
+
+    def on_selected_student(self, instance, student):
+        self.root.skills_screen.student_name.text = "{} {}".format(student.surname,
+                                                                   student.name)
+
+    def open_skills_popup(self):
+        Popup(content=SkillsPopupLayout(cols=2,
+              skills_list=self.skills_list,
+              app=self),
+              title='Faites votre choix',
+              size_hint_y=0.5).open()
+
+    def on_selected_skill(self, instance, skill):
+        self.root.skills_screen.skill_name.text = skill.title
+        self.root.skills_screen.skill_summary.text = skill.summary
+
+    def set_student_skill(self, value):
+        try:
+            student = self.selected_student.id_
+            output = self.data_output
+            skill = self.selected_skill.title
+            try:
+                length = len(output[student][skill])
+                output[student][skill][length] = value
+            except KeyError:
+                try:
+                    output[student][skill] = {0: value}
+                except KeyError:
+                    output[student] = self.students_data[student]
+            output._is_changed = True
+            output.store_sync()
+        except AttributeError:
+            pass
 
     def on_pause(self):
         return True
