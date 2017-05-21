a9cefee2239d69233a550738076533c86ae0ff74
Maxime Ertul <maxime.ertul@gmail.com>
2017-05-21 02:33:27 +0200

    Added a scrollable label to the start screen

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # coding: utf8
 from kivy.app import App
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import ObjectProperty
 from kivy.storage.jsonstore import JsonStore
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
 
 
 class Skill(object):
@@ -30,9 +32,9 @@ class CustomDropDown(DropDown):
 
 
 class SkillsPopupLayout(GridLayout):
-    def __init__(self, app, skills_list, **kwargs):
+    def __init__(self, root, skills_list, **kwargs):
         super().__init__(**kwargs)
-        self.app = app
+        self.root = root
         self.skills_list = skills_list
         self.btn_dict = {}
         for s in self.skills_list:
@@ -42,14 +44,14 @@ class SkillsPopupLayout(GridLayout):
             self.btn_dict[button] = s
 
     def select(self, obj):
-        self.app.selected_skill = self.btn_dict[obj]
+        self.root.selected_skill = self.btn_dict[obj]
         self.parent.parent.parent.dismiss()
 
 
 class StudentPopupLayout(GridLayout):
-    def __init__(self, app, student_list, **kwargs):
+    def __init__(self, root, student_list, **kwargs):
         super().__init__(**kwargs)
-        self.app = app
+        self.root = root
         self.student_list = student_list
         self.btn_dict = {}
         for s in self.student_list:
@@ -59,7 +61,7 @@ class StudentPopupLayout(GridLayout):
             self.btn_dict[button] = s
 
     def select(self, obj):
-        self.app.selected_student = self.btn_dict[obj]
+        self.root.selected_student = self.btn_dict[obj]
         self.parent.parent.parent.dismiss()
 
 
@@ -82,10 +84,18 @@ class MenuButton(Button):
                 instance.add_widget(button)
 
 
-class Root(BoxLayout):
+class StartScreen(Screen):
+    pass
 
-    def __init__(self, **kwargs):
+
+class Root(BoxLayout):
+    selected_student = ObjectProperty()
+    selected_skill = ObjectProperty()
+
+    def __init__(self, students, skills, **kwargs):
         super().__init__(**kwargs)
+        self.student_list = students
+        self.skill_list = skills
         self.screen_list = ['Accueil', 'Evaluation', 'Comportement']
         self.menu.drop_down.bind(on_select=self.select_screen)
 
@@ -93,20 +103,36 @@ class Root(BoxLayout):
         self.screen_manager.current = value
         self.menu.update_menu(instance, value, self.screen_list)
 
+    def open_student_popup(self):
+        Popup(content=StudentPopupLayout(self,
+              self.student_list,
+              cols=2),
+              title='Faites votre choix',
+              auto_dismiss=False).open()
+
+    def on_selected_student(self, instance, student):
+        self.skills_screen.student_name.text = "{} {}".format(student.surname,
+                                                              student.name)
+
+    def open_skills_popup(self):
+        Popup(content=SkillsPopupLayout(self,
+              self.skill_list,
+              cols=2),
+              title='Faites votre choix',
+              size_hint_y=0.5).open()
+
+    def on_selected_skill(self, instance, skill):
+        self.skills_screen.skill_name.text = skill.title
+        self.skills_screen.skill_summary.text = skill.summary
+
 
 class ProjectZApp(App):
     students_data = ObjectProperty(JsonStore('data/students.json'))
     skills_data = ObjectProperty(JsonStore('data/skills.json'))
     data_output = ObjectProperty(JsonStore('data/outputs.json'))
-    student_list = ListProperty()
-    skills_list = ListProperty()
-    selected_student = ObjectProperty()
-    selected_skill = ObjectProperty()
 
     def build(self):
-        self.student_list = self.get_students()
-        self.skills_list = self.get_skills()
-        self.root = Root()
+        self.root = Root(self.get_students(), self.get_skills())
         return self.root
 
     def get_students(self):
@@ -124,33 +150,11 @@ class ProjectZApp(App):
             file.close()
         return path
 
-    def open_student_popup(self):
-        Popup(content=StudentPopupLayout(self,
-              self.student_list,
-              cols=2),
-              title='Faites votre choix',
-              auto_dismiss=False).open()
-
-    def on_selected_student(self, instance, student):
-        self.root.skills_screen.student_name.text = "{} {}".format(student.surname,
-                                                                   student.name)
-
-    def open_skills_popup(self):
-        Popup(content=SkillsPopupLayout(cols=2,
-              skills_list=self.skills_list,
-              app=self),
-              title='Faites votre choix',
-              size_hint_y=0.5).open()
-
-    def on_selected_skill(self, instance, skill):
-        self.root.skills_screen.skill_name.text = skill.title
-        self.root.skills_screen.skill_summary.text = skill.summary
-
     def set_student_skill(self, value):
         try:
-            student = self.selected_student.id_
+            student = self.root.selected_student.id_
             output = self.data_output
-            skill = self.selected_skill.title
+            skill = self.root.selected_skill.title
             try:
                 length = len(output[student][skill])
                 output[student][skill][length] = value
@@ -163,6 +167,36 @@ class ProjectZApp(App):
             output.store_sync()
         except AttributeError:
             pass
+
+    def display_skills(self):
+        self.root.start_screen.display_label.clear_widgets()
+        output = self.data_output
+        data = {}
+        header = ["prenom"]
+        header.extend([skill.title for skill in self.root.skill_list])
+        grid_layout = GridLayout(cols=len(header), size_hint_y=None)
+        grid_layout.bind(minimum_height=grid_layout.setter('height'))
+        [grid_layout.add_widget(Label(text=item[:4], size_hint_y=None,
+                                      height='40dp',
+                                      font_size='12sp')) for item in header]
+        for key in output:
+            data[key] = output[key]
+            try:
+                data[key].pop('class')
+                data[key].pop('name')
+            except KeyError:
+                pass
+            row = [data[key]["surname"]]
+            for skill in header[1:]:
+                try:
+                    item = data[key][skill]
+                    row.append(", ".join(str(item[i]) for i in item))
+                except KeyError:
+                    row.extend(["S/O"])
+            [grid_layout.add_widget(Label(text=item, size_hint_y=None,
+                                          height='40dp',
+                                          font_size='12sp')) for item in row]
+        self.root.start_screen.display_label.add_widget(grid_layout)
 
     def on_pause(self):
         return True
